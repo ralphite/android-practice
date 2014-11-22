d096ffb89f9e55e141cd790ba2ab1e2babfad4f3
Yadong Wen <ralph.wen@gmail.com>
2014-11-22 15:51:13 +0800

    init

---
+++ src/com/ralphwen/criminalintent/Crime.java
@@ -0,0 +1,24 @@
+package com.ralphwen.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+	private UUID mId;
+	private String mTitle;
+	
+	public Crime() {
+		mId = UUID.randomUUID();
+	}
+
+	public String getTitle() {
+		return mTitle;
+	}
+
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+
+	public UUID getId() {
+		return mId;
+	}
+}
