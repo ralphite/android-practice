49142636d92b655be2b72e4a2cf4948f027f4b97
Yadong Wen <ralph.wen@gmail.com>
2014-11-22 22:48:59 +0800

    use ui fragment

---
+++ src/com/ralphwen/criminalintent/CrimeFragment.java
@@ -0,0 +1,54 @@
+package com.ralphwen.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+	private Crime mCrime;
+	private EditText mTitleField;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mCrime = new Crime();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+		mTitleField = (EditText) v.findViewById(R.id.crime_title);
+		mTitleField.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence cs, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				mCrime.setTitle(cs.toString());
+			}
+
+		});
+
+		return v;
+	}
+}
