ed9899164e46e3b911590cef51af7af2dd4fd33d
Yadong Wen <ralph.wen@gmail.com>
2014-11-23 23:59:05 +0800

    page 168

--- src/com/ralphwen/criminalintent/CrimeFragment.java
+++ src/com/ralphwen/criminalintent/CrimeFragment.java
@@ -3,11 +3,13 @@ package com.ralphwen.criminalintent;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.UUID;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +18,10 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
-import android.text.format.DateFormat;
 
 public class CrimeFragment extends Fragment {
+	public static String EXTRA_CRIME_ID = "com.ralphwen.criminalintent.crime_id";
+
 	private Crime mCrime;
 	private EditText mTitleField;
 	private Button mDateButton;
@@ -27,7 +30,10 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mCrime = new Crime();
+		UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(
+				EXTRA_CRIME_ID);
+		
+		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
 	}
 
 	@Override
@@ -36,6 +42,7 @@ public class CrimeFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 
 		mTitleField = (EditText) v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
 		mTitleField.addTextChangedListener(new TextWatcher() {
 
 			@Override
@@ -69,6 +76,7 @@ public class CrimeFragment extends Fragment {
 		mDateButton.setEnabled(false);
 
 		mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setChecked(mCrime.isSolved());
 		mSolvedCheckBox
 				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
