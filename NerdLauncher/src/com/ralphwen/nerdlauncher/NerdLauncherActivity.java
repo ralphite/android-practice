e6f403dd716e475b7718f8ed874fb4607305d414
Yadong Wen <ralph.wen@gmail.com>
2014-12-04 20:12:38 +0800

    page 338

---
+++ NerdLauncher/src/com/ralphwen/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,15 @@
+package com.ralphwen.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+	@Override
+	public Fragment createFragment() {
+		return new NerdLauncherFragment();
+	}
+}
