44e33f6dbca284e7be03fc4c9001d76869712e43
Yadong Wen <ralph.wen@gmail.com>
2014-12-17 17:18:21 +0800

    page 449

--- PhotoGallery/app/src/main/java/com/ralphwen/photogallery/GalleryItem.java
+++ PhotoGallery/app/src/main/java/com/ralphwen/photogallery/GalleryItem.java
@@ -7,6 +7,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     public String getCaption() {
         return mCaption;
@@ -36,4 +37,15 @@ public class GalleryItem {
         return mCaption;
     }
 
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public String getPhotoPageUrl() {
+        return "http://www.flickr.com/photos/" + mOwner + "/" + mId;
+    }
 }
--- PhotoGallery/app/src/main/java/com/ralphwen/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/ralphwen/photogallery/PhotoGalleryFragment.java
@@ -6,7 +6,9 @@ import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,6 +22,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -27,9 +30,6 @@ import android.widget.SearchView;
 
 import java.util.ArrayList;
 
-/**
- * Created by Yadong on 12/9/2014.
- */
 public class PhotoGalleryFragment extends VisibleFragment {
 
     //log tag
@@ -73,12 +73,25 @@ public class PhotoGalleryFragment extends VisibleFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 
         mGridView = (GridView) v.findViewById(R.id.gridView);
 
         setupAdapter();
+
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                GalleryItem item = mItems.get(position);
+
+                Uri photoPageUri = Uri.parse(item.getPhotoPageUrl());
+                Intent i = new Intent(Intent.ACTION_VIEW, photoPageUri);
+
+                startActivity(i);
+            }
+        });
         return v;
     }
 
