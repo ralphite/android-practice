85f01d0a1fe997cc16ba52e2283ffb71c4d9c530
Yadong Wen <ralph.wen@gmail.com>
2014-12-22 10:11:24 +0800

    solved 8.6

--- CriminalIntent3/app/src/main/java/com/ralphwen/criminalintent3/CrimeFragment.java
+++ CriminalIntent3/app/src/main/java/com/ralphwen/criminalintent3/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,7 +55,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setText(DateFormat.format("EEEE, MMM dd, yyyy", mCrime.getDate()));
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
