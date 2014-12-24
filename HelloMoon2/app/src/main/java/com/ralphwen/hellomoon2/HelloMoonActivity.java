699a8a602a1445840f7c5e7ebb40d44f66da146e
Yadong Wen <ralph.wen@gmail.com>
2014-12-24 21:13:05 +0800

    half solved 13.8

--- HelloMoon2/app/src/main/java/com/ralphwen/hellomoon2/HelloMoonActivity.java
+++ HelloMoon2/app/src/main/java/com/ralphwen/hellomoon2/HelloMoonActivity.java
@@ -1,40 +1,107 @@
 package com.ralphwen.hellomoon2;
 
-import android.support.v4.app.FragmentActivity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TabHost;
+
+import java.io.IOException;
 
 
 public class HelloMoonActivity extends ActionBarActivity {
+
+    public static final String TAG = "HelloMoonActivity";
+
+    private AudioPlayer mAudioPlayer;
+    private MediaPlayer mMediaPlayer, holdOn;
+    private SurfaceView mSurfaceView;
+    private Uri mUri = Uri.parse("android.resource://" +
+            "com.ralphwen.hellomoon2/raw/apollo_17_stroll");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_moon);
-    }
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec audioSpec = tabHost.newTabSpec("play_audio");
+        audioSpec.setIndicator("Audio");
+        audioSpec.setContent(R.id.audio);
+        tabHost.addTab(audioSpec);
+
+        TabHost.TabSpec videoSpec = tabHost.newTabSpec("play_video");
+        videoSpec.setIndicator("Video");
+        videoSpec.setContent(R.id.video);
+        tabHost.addTab(videoSpec);
+
+        mAudioPlayer = new AudioPlayer();
+
+        Button mAudioPlayButton = (Button)findViewById(R.id.audio_playButton);
+        mAudioPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAudioPlayer.play(getApplication());
+            }
+        });
+
+        Button mAudioPauseButton = (Button) findViewById(R.id.audio_pauseButton);
+        mAudioPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAudioPlayer.pause(getApplication());
+            }
+        });
+
+        Button mAudioStopButton = (Button) findViewById(R.id.audio_stopButton);
+        mAudioStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAudioPlayer.stop();
+            }
+        });
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hello_moon, menu);
-        return true;
-    }
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.apollo_17_stroll);
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                holdOn = mp;
+            }
+        });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        mSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                mMediaPlayer.setDisplay(holder);
+                try {
+                    //mMediaPlayer.setDataSource(getApplicationContext(), mUri);
+                    mMediaPlayer.prepare();
+                    mMediaPlayer.start();
+                } catch (IOException ioe) {
+                    Log.e(TAG, "Error opening video file.", ioe);
+                }
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
-        return super.onOptionsItemSelected(item);
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mMediaPlayer.release();
+            }
+        });
+
+
     }
 }
